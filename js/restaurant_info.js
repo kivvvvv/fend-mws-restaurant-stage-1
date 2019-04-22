17bd6afec083cdb4b38a72ccd3b9f48b69397000
kivvvvv <kivistheslayer@gmail.com>
2019-04-22 07:34:51 +0700

    Init student's work. Added student's Mapbox keys.

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1IjoiYmlsbHktYm95IiwiYSI6ImNqdXFnNGE2NTB6MmgzeW1rdGlmZGpsa3kifQ.1MErJ3rnlyezDA4_7ojbMg',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
